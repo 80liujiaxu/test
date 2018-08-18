717dfed2a647005b7d02cac3f8dc8bcc7075e0f7
80946 <80946@10.30.38.73>
2018-08-18 09:13:14 +0800

    第一次提交

---
+++ src/com/test/demo/dan.java
@@ -0,0 +1,18 @@
+package com.test.demo;
+
+public class dan {
+
+	
+	
+	 private static dan eHan;
+
+	private  dan() {}
+	
+	public static dan goEhan()
+	{
+		if(eHan==null)
+		{
+		}
+		return eHan;	
+	}
+}
